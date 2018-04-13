734b66c43ec5c5b702c1efb1290ac9ba0e1654b4
JoeRoc13 <joseph.rocaberte@gmail.com>
2018-04-13 16:32:51 -0400

    Updates

--- API/login.php
+++ API/login.php
@@ -9,11 +9,23 @@
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($password == $row['password']){
-        echo "Logged in";
+    if($password == $row["password"]){
+        $result = array(
+          "success" => true,
+          "uid" => $row["uid"],
+          "username" => $row["username"],
+          "email" => $row["email"],
+          "location" => $row["location"],
+          "regis_date" => $row["regis_date"]
+        );
     } else {
-      echo "Wrong Password";
+      $result = array(
+        "success" => false,
+        "error" => "Incorrect username or password"
+      );
     }
+
+    echo json_encode($result);
   }
 
 ?>
